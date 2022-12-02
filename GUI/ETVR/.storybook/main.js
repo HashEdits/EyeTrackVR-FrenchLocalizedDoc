a6ebe614f59981089654c62c0e38d2a7c4074738
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-12-02 18:05:46 +0000

    update - begin creating flowchart for app architecture - remove db.json - add storybook addon-docs - implement MDX support

--- GUI/ETVR/.storybook/main.js
+++ GUI/ETVR/.storybook/main.js
@@ -4,21 +4,33 @@ require("storybook-tailwind-foundations/initialize.js").default(
     tailwindConfigPath
 );
 
+//* Docs: https://storybook.js.org/docs/react/essentials/controls
+//* Docs: https://storybook.js.org/docs/react/writing-docs/mdx#gatsby-focus-wrapper
+//* Docs: https://storybook.js.org/addons/@storybook/addon-docs
+
 const resolve = (item) => {
     return path.join(__dirname, "../", item);
 };
 const { mergeConfig } = require("vite");
 
 module.exports = {
-    stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-    addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+    stories: ["../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
+    addons: [{
+        name: '@storybook/addon-docs',
+        options: {
+            configureJSX: true,
+            babelOptions: {},
+            sourceLoaderOptions: null,
+            transcludeMarkdown: true,
+        },
+    }, "@storybook/addon-links", "@storybook/addon-essentials"],
 
     framework: "@storybook/react",
     core: {
         builder: "@storybook/builder-vite",
     },
 
-    viteFinal(config, {}) {
+    viteFinal(config, { }) {
         return mergeConfig(config, {
             resolve: {
                 alias: {
